7fc6ed956ab5e827e9c32084340cc14d74fbc692
Your Name <you@example.com>
2023-05-22 21:59:09 +0200

    push tuto

---
+++ forge.config.js
@@ -0,0 +1,41 @@
+require('dotenv').config();
+
+module.exports = {
+  packagerConfig: {},
+  rebuildConfig: {},
+  makers: [
+    {
+      name: '@electron-forge/maker-squirrel',
+      config: {
+        certificateFile: './cert.pfx',
+        certificatePassword: process.env.CERTIFICATE_PASSWORD,
+      },
+    },
+    {
+      name: '@electron-forge/maker-zip',
+      platforms: ['darwin'],
+    },
+    {
+      name: '@electron-forge/maker-deb',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-rpm',
+      config: {},
+    },
+  ],
+  publishers: [
+    {
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'groudse',
+          name: 'tuto_electron',
+        },
+        prerelease: false,
+        draft: true,
+        token: process.env.GITHUB_TOKEN
+      },
+    },
+  ]
+};
---
+++ main.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow, ipcMain } = require('electron')
+const path = require("path");
+require('dotenv').config();
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+        },
+    })
+    ipcMain.handle('ping', () => 'pong')
+    win.loadFile('index.html')
+}
+console.log(process.env.GITHUB_TOKEN)
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow()
+        }
+    })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
